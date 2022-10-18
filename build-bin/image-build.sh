6b5c88178ad909f3f5af83cd72ccfb6f00635ade
gissily <gissily@outlook.com>
2022-10-19 00:05:32 +0800

    fix GITHUB_WORKSPACE

--- build-bin/image-build.sh
+++ build-bin/image-build.sh
@@ -11,13 +11,12 @@ echo "build kafka:${SCALA_VERSION}-${KAFKA_VERSION} start"
 docker build \
     --build-arg scala_version=${SCALA_VERSION} \
     --build-arg kafka_version=${KAFKA_VERSION} \
-    -t kafka:${SCALA_VERSION}-${KAFKA_VERSION}-${TAG_VERSION} \
-    -f ${CI_PROJECT_DIR}/Dockerfile . --no-cache
-docker image tag kafka:${SCALA_VERSION}-${KAFKA_VERSION}-${TAG_VERSION} ${CI_REGISTRY}/opcal-project/containers/kafka:${RELEASE_VERSION}
-docker push ${CI_REGISTRY}/opcal-project/containers/kafka:${RELEASE_VERSION}
+    -t kafka:${SCALA_VERSION}-${KAFKA_VERSION} \
+    -f ${GITHUB_WORKSPACE}/Dockerfile . --no-cache
+docker image tag kafka:${SCALA_VERSION}-${KAFKA_VERSION} ${CI_REGISTRY}/opcal/kafka:${RELEASE_VERSION}
+docker push ${CI_REGISTRY}/opcal/kafka:${RELEASE_VERSION}
 
 echo "build kafka:${SCALA_VERSION}-${KAFKA_VERSION} finished"
-
 
 echo 'build kafka finished'
 echo " "
