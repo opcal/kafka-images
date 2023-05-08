7f474a4b4098e17110c9c972f2c2f422af3a1e7d
gissily <gissily@outlook.com>
2023-05-09 01:08:55 +0800

    fix tags

--- build-bin/image-build.sh
+++ build-bin/image-build.sh
@@ -11,9 +11,9 @@ echo "build kafka:${SCALA_VERSION}-${KAFKA_VERSION} start"
 docker build \
     --build-arg scala_version=${SCALA_VERSION} \
     --build-arg kafka_version=${KAFKA_VERSION} \
-    -t kafka:${SCALA_VERSION}-${KAFKA_VERSION} \
+    -t ${CI_REGISTRY}/opcal/kafka:${RELEASE_VERSION} \
     -f ${PROJECT_DIR}/Dockerfile . --no-cache
-docker image tag kafka:${SCALA_VERSION}-${KAFKA_VERSION} ${CI_REGISTRY}/opcal/kafka:${RELEASE_VERSION}
+
 docker push ${CI_REGISTRY}/opcal/kafka:${RELEASE_VERSION}
 
 echo "build kafka:${SCALA_VERSION}-${KAFKA_VERSION} finished"
